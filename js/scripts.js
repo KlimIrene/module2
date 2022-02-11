a47f293c5c0a63187d305f6961d17d6e7ba958ea
KlimIrene <irene.klim88@gmail.com>
2022-02-11 21:36:13 +0200

    responsive

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$('.checkin input[type=data]').each(function(){
+    var input = $(this); // This is the jquery object of the input, do what you will
+   });
+
+$('.checkin input:button').click(function(){
+    // $('.choose select').slideDown('slow')
+    // $('input').slideDown('slow')
+
+    $('send').text()
+})
