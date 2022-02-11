5c5ce89df35477421996fa25ebc39889acbba298
KlimIrene <irene.klim88@gmail.com>
2022-02-11 22:24:54 +0200

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-$('.checkin input[type=data]').each(function(){
-    var input = $(this); // This is the jquery object of the input, do what you will
-   });
 
-$('.checkin input:button').click(function(){
-    // $('.choose select').slideDown('slow')
-    // $('input').slideDown('slow')
+$(".btn").click(function(){
+    $("html, body").animate({scrollTop: $("header").height()+ 1800 },"slow");
+    var valPlaces = $('select.places option:checked').val()
+    $('#check-holder').text(valPlaces);
+});
 
-    $('send').text()
-})
+// var valPlaces =    $('select.places option:checked').val()
+// $('#check-holder').text(valPlaces);
+
+// var valCheckIn =    $('input.check-in option:checked').val()
+// $('#check-holder').text(valCheckIn);
+
+// var valCheckOut =    $('input.check-out option:checked').val()
+// $('#check-holder').text(valCheckOut);
+
+// var valRooms =    $('select.rooms option:checked').val()
+// $('#check-holder').text(valRooms);
